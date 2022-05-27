bb433f6bafecc7c10652ff789b16a236e30fd3be
MihaTeam1 <51956107+MihaTeam1@users.noreply.github.com>
2022-05-28 00:17:09 +0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 commands = '/home/www/code/project/env/bin/gunicorn'
 pythonpath = 'home/www/code/project/project'
 bind = '127.0.0.1:8001'
-# bind = 'unix:/run/gunicorn.sock'
+# bind = 'unix:/run/gunicorn.sock' #Если предпочтение соккетам а не tcp/ip
 # worker_class = 'uvicorn.workers.UvicornWorker'
 workers = 3 # Количество ядер * 2 + 1
 limit_request_fields = 32000
