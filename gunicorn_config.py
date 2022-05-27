7e6ac0f54711e929aa7c4e481f3af129f6d71139
MihaTeam1 <51956107+MihaTeam1@users.noreply.github.com>
2022-05-27 19:36:41 +0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,4 +6,4 @@ bind = '127.0.0.1:8001'
 workers = 3 # Количество ядер * 2 + 1
 limit_request_fields = 32000
 limit_request_fields_size = 0
-raw_dnv = 'DJANGO_SETTINGS_MODULE=project.settings'
+raw_env = 'DJANGO_SETTINGS_MODULE=project.settings'
