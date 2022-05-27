24963b463cfd91a0eef3fcf079417fc1e72527f9
MihaTeam1 <51956107+MihaTeam1@users.noreply.github.com>
2022-05-27 17:53:29 +0300

    Update start_celery.sh

--- start_celery.sh
+++ start_celery.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source /home/www/code/project/env/bin/activate
 cd /home/www/code/project/project
-exec python -m celery -A project_name worker --beat -l INFO --concurrency=3 --max-tasks-pre-child=1
+exec python -m celery -A project_name worker --beat -l INFO --concurrency=3 --max-tasks-per-child=1
+# --concurrency количество потоков у воркера (формула: количество ядер * 2 + 1)
+# --max-tasks-per-child максимальное количество задач которое выполнит 1 поток селери перед перезагрузкой
